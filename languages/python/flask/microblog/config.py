9441d44e29cb5f3080ae18627e2af37a2924016f
Sylvestre <sylvestre.antoine+gpg@gmail.com>
2020-02-08 12:46:53 +0100

    [python][Flask] microblog. database

--- languages/python/flask/microblog/config.py
+++ languages/python/flask/microblog/config.py
@@ -2,5 +2,10 @@ import os
 
 def raise_KeyError(msg=''): raise KeyError(msg)
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
-  SECRET_KEY = os.environ.get('SECRET_KEY') or raise_KeyError('Secret key is missing')
+  SECRET_KEY = os.environ.get('SECRET_KEY') or raise_KeyError('SECRET_KEY is missing')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(base_dir, 'microblog.sqlite')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False # signal the app when change done in DB
--- languages/python/flask/microblog/install.sh
+++ languages/python/flask/microblog/install.sh
@@ -3,10 +3,20 @@
 python3 -m venv venv
 source venv/bin/activate
 
+pip install --upgrade pip
 pip install flask
 pip install python-dotenv
 pip install flask-wtf
+pip install flask-sqlalchemy
+pip install flask-migrate
+pip install flask-shell-ipython
 
+# flask db init
+# flask db migrate -m "users table"
+# flask db migrate -m "posts table"
+# flask db upgrade # flask db downgrade #   Apply/remove to the DB
 # flask run
 
 # launch deactivate to quit the virtual env
+# console + do import : dotenv run python
+# console with flask loaded : flask shell
